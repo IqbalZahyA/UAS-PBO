91a0f7f5384907ff6735e66b8c12aba12ec81c24
ThreeGiee <ajisupandi166@gmail.com>
2025-06-15 18:43:05 +0700

    dengan sql

--- uaspbo/Koneksi.java
+++ uaspbo/Koneksi.java
@@ -1,23 +1,24 @@
+package uaspbo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Koneksi {
-    private static Connection Koneksi;
+    private static Connection koneksi;
 
     public static Connection getKoneksi() {
-        if (Koneksi == null) {
+        if (koneksi == null) {
             try {
                 // Load driver JDBC MySQL modern
                 Class.forName("com.mysql.cj.jdbc.Driver");
 
                 // Konfigurasi database
-                String url = "jdbc:mysql://localhost:3306/tokobuku1";
+                String url = "jdbc:mysql://localhost:3306/uaspbo";
                 String user = "root";
                 String password = "";
 
-                Koneksi = DriverManager.getConnection(url, user, password);
+                koneksi = DriverManager.getConnection(url, user, password);
                 System.out.println("Koneksi database berhasil!");
             } catch (ClassNotFoundException e) {
                 System.out.println("JDBC Driver tidak ditemukan: " + e.getMessage());
@@ -25,7 +26,7 @@ public class Koneksi {
                 System.out.println("Gagal koneksi database: " + e.getMessage());
             }
         }
-        return Koneksi;
+        return koneksi;
     }
 
     public static void main(String[] args) {
---
+++ uaspbo/LoginSession.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uaspbo;
+
+/**
+ *
+ * @author User
+ */
+public class LoginSession {
+    private static String idMember;
+
+    public static void setIdMember(String id) {
+        idMember = id;
+    }
+
+    public static String getIdMember() {
+        return idMember;
+    }
+
+}
